7e3d3d22ce8a39967ef3fc9e19a387ebc7344914
Rightpeter <rightpeter.lu@gmail.com>
2019-01-04 11:02:43 +0800

    Refactory

--- cube_demo_system/urls.py
+++ cube_demo_system/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('visualize/', views.visualize, name='visualize'),
     path('visualize/', include('visualization.urls')),
     path('cube/', include('cube.urls')),
-    path('', include('users.urls')),
+    path('', views.HomePageView.as_view()),
 ]
